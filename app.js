39217a2e3fd0c1168cda7f9d7be18144835e57c9
amaralp <dogi12@hotmail.com>
2022-07-18 00:15:40 -0500

    queue finished

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ class Item {
     }
 }
 
+
 class PriorityQueue {
     constructor(){
         this.items = [];
@@ -21,26 +22,80 @@ class PriorityQueue {
             }
         }
      
-        if (!positionFound) {
+        if (positionFound === false) {
             this.items.push(item);
         }
 
+    }
+
+    delete(){
+        if (!this.isEmpty())
+            return this.items.pop();
     }
 
     isEmpty(){
         return this.items.length == 0;
     }
 
-    delete(){
-        if (this.isEmpty())
-            return "Underflow";
-            return this.items.shift();
-    }
     print(){
         let queue = "";
-        for (var i = 0; i < this.items.length; i++)
-            queue += this.items[i].element + " ";
+        for (let i = 0; i < this.items.length; i++)
+            queue += this.items[i].value + ", ";
         return queue; 
     }
 }
 
+// ------------------- queue 1 -----------------------
+console.log("------------------- queue 1 -----------------------")
+let priorityQueue = new PriorityQueue();
+
+console.log(priorityQueue.isEmpty());
+
+priorityQueue.insert("one", 1);
+priorityQueue.insert("three", 3);
+priorityQueue.insert("four", 4);
+priorityQueue.insert("six", 6);
+priorityQueue.insert("two", 2);
+priorityQueue.insert("five", 5);
+priorityQueue.insert("seventy", 70);
+priorityQueue.insert("one-hundred", 100);
+priorityQueue.insert("zero", 0);
+
+console.log(priorityQueue.print());
+
+priorityQueue.delete();
+console.log(priorityQueue.print());
+
+priorityQueue.delete();
+console.log(priorityQueue.print());
+
+priorityQueue.delete();
+console.log(priorityQueue.print());
+
+priorityQueue.delete();
+console.log(priorityQueue.print());
+
+priorityQueue.insert("three2", 3);
+console.log(priorityQueue.print());
+
+// ------------------- queue 2 -----------------------
+console.log("------------------- queue 2 -----------------------")
+let priorityQueue2 = new PriorityQueue();
+
+console.log(priorityQueue2.isEmpty());
+
+// adding elements to the queue
+priorityQueue2.insert("one", 1);
+priorityQueue2.insert("one-hundred", 100);
+priorityQueue2.insert("zero", 0);
+
+console.log(priorityQueue2.print());
+
+priorityQueue2.delete();
+priorityQueue2.delete();
+priorityQueue2.delete();
+priorityQueue2.delete();
+console.log(priorityQueue2.print());
+
+priorityQueue2.insert("three", 3);
+console.log(priorityQueue2.print());
